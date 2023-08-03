92d823abc0da8f434616a4c9109b3eb7780be9e2
Pkchess2008 <95050254+pkchess2008@users.noreply.github.com>
2023-08-02 20:27:23 -0700

    Update Program02.java

--- src/session07/Program02.java
+++ src/session07/Program02.java
@@ -1,9 +1,51 @@
 package session07;
 
+
+/**
+ * Program to prompt and accept the computer marks of n students of a class.
+ * Find the highest marks.
+ * 
+ */
+import java.util.Scanner;
 public class Program02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//declaration
+		Scanner bot = new Scanner(System.in);
+		int n;
+		double[] marks;
+		double highestMarks;
+		
+		//Input
+		System.out.println("Enter the number of students in the class: ");
+		n = bot.nextInt();
+		
+		//Static initialization
+		marks = new double[n];
+		
+		//Input
+		for (int i = 0; i <n;i++) {
+			System.out.println("What is the mark of student " + (i+1) + ": ");
+			marks[i] = bot.nextDouble();
+		}
+		
+		//Computation
+		highestMarks = marks[0];
+		
+		for (int i = 1; i < n; i++) 
+		{
+		    if (marks[i] > highestMarks)
+			{
+				highestMarks = marks[i];
+				
+			}
+		}
+		
+		//Output 
+		
+		
+		
 
 	}
 
